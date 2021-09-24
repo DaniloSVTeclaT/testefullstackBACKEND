ed6dffce9727efb84b8ba3baa11e2a2c1a1cd4ac
Danilo Tecla T <danilo.sampaio@teclat.com.br>
2021-09-24 14:41:30 -0300

    backend

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: 'b0dd8bbc0b53f4a5599d72c5d024e881',
+        expiresIn: '1d'
+    }
+};
