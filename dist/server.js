a9d43f2e50e1032c126d4e8c667af4d2f5b76c05
Danilo Tecla T <danilo.sampaio@teclat.com.br>
2021-09-25 02:15:00 -0300

    configurando banco

--- dist/server.js
+++ dist/server.js
@@ -26,6 +26,6 @@ app.use(function (err, request, response, next) {
         message: 'Internal server error'
     });
 });
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Server start sucess on port 3000');
 });
