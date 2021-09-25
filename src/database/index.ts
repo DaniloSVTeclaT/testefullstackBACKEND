fe9dffee062b4eb4d685bab7b5ac052ec396834c
Danilo Tecla T <danilo.sampaio@teclat.com.br>
2021-09-25 12:46:09 -0300

    atz

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@ import {createConnection} from 'typeorm'
 
 createConnection({
     type:"postgres",
-    url: process.env.DATABASE_URL,
+    url: "postgres://hzmnexneamkmhj:8554010eba235f2934740bfcfe3332d167b499aa9e6754bd16eba6a816bcab45@ec2-3-220-214-162.compute-1.amazonaws.com:5432/danlnpamj081ec",
     synchronize: false,
     logging: true,
     extra:{
