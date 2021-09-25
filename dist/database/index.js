8b4fbd94a52aa31985785a668c5bcbb59446dfab
Danilo Tecla T <danilo.sampaio@teclat.com.br>
2021-09-25 12:31:10 -0300

    atz

--- dist/database/index.js
+++ dist/database/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var typeorm_1 = require("typeorm");
 (0, typeorm_1.createConnection)({
     type: "postgres",
-    url: DB_URL,
+    url: DATABASE_URL,
     synchronize: false,
     logging: true,
     extra: {
