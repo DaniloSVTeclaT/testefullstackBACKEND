61e174b97239d6b52c3bc94509a4557cd1c1fcad
Danilo Tecla T <danilo.sampaio@teclat.com.br>
2021-09-25 16:09:24 -0300

    atz

--- src/server.ts
+++ src/server.ts
@@ -27,5 +27,5 @@ app.use((err:Error,request:Request,response:Response,next:NextFunction)=>{
 })
 
 app.listen(process.env.PORT ||5000,()=>{
-    console.log('Server start sucess on port 3333')
+    console.log('Server start sucess on port 3333'+process.env.DATABASE_URL)
 })
